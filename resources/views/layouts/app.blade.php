ff0d64e5f62c797ff72a0b2c041c854353469e58
vagrant <vagrant@homestead>
2018-11-16 08:02:55 +0000

    simditor editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <title>@yield('title','larabbs')</title>
 
     <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+    @yield('styles')
 </head>
 <body>
     <div id="app" class="{{ route_class()}}-page">
@@ -22,5 +23,6 @@
     </div>
 
     <script src="{{asset('js/app.js')}}"></script>
+    @yield('scripts')
 </body>
 </html>
