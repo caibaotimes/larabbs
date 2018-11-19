eef87579417f9acf4ad7adc855215449d67c7755
vagrant <vagrant@homestead>
2018-11-19 07:22:22 +0000

    auto translate slug

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -26,7 +26,8 @@ class TopicsController extends Controller
 
     public function show(Topic $topic)
     {
-        return view('topics.show', compact('topic'));
+        // return view('topics.show', compact('topic'));
+        return redirect()->to($topic->link());
     }
 
 	public function create(Topic $topic)
@@ -41,7 +42,8 @@ class TopicsController extends Controller
         $topic->user_id = Auth::id();
         $topic->save();
 
-		return redirect()->route('topics.show', $topic->id)->with('success', '创建成功');
+		// return redirect()->route('topics.show', $topic->id)->with('success', '创建成功');
+        return redirect()->to($topic->link())->with('success','创建成功');
 	}
 
 	public function edit(Topic $topic)
@@ -56,7 +58,8 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('success', '更新成功');
+		// return redirect()->route('topics.show', $topic->id)->with('success', '更新成功');
+        return redirect()->to($topic->link())->with('success','更新成功');
 	}
 
 	public function destroy(Topic $topic)
